ba973da23ce76a303b6d64220dfb03d217922d64
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-18 13:21:59 +0500

    serverless function calling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,24 @@
-import React from "react"
-import { Link } from "gatsby"
+import React,{useState,useEffect} from "react"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+let  IndexPage = () => {
+  const [state, setstate] = useState("");
+useEffect(() => {
+  (async ()=>{
+          let api =await fetch(".netlify/functions/hello_world")
+             let data=   await api.json();
+             console.log("Data", data)
+             setstate(data.message);
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi  people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  }
+
+  )()
+}, [])
+
+return(
+
+  <h1>Heelo World {state }</h1>
+);
+
+}
 
 export default IndexPage
